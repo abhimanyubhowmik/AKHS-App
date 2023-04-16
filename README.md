7836ce63d4de0bf33fa1691e9d6d836ea6aced94
Abhimanyu Bhowmik <bhowmikabhimanyu@gmail.com>
2023-04-16 14:51:03 +0530

    Primary Commit

--- README.md
+++ README.md
@@ -1,70 +1,30 @@
-# Getting Started with Create React App
+# Ariadaha Kalachand High School (AKHS App)
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This is a demo app build by me in April 2021 on request of one of my friend. It's tribute to my school from my end. This is a single page basic website with potential to be extended into multipage app for displaying information, courses and releted sutffs.
 
-## Available Scripts
+## Overview of the School
 
-In the project directory, you can run:
+For more than a century, Ariadaha Kalachand High School has been standing as an epitome of the aspirations of the people of Ariadaha-Dakshineswar in the northern suburb of Kolkata.The school overlooks the Feeder Road that connects Belghoria Railway Station on Sealdah-Ranaghat section with the Ariadaha Ferry Ghat on the riverbank of the Hooghly. The junction in front of the school branches out towards Dakshineswar Railway station and ultimately reaches out to the Dakshineswar Kali Temple. The school has produced a plethora of students who have been successful in their own fields and have contributed lots to the nation in their own way. They have all done the school proud.
 
-### `npm start`
+AKHS was set up on 6th January 1884 on the bank of the holy river Ganga, being inspired by the ideals of Pandit Iswar Chandra Vidyasagar and Reverend Krishnamohan Bandyopadhyay and guided by the spirit of the ‘Young Bengal' against the backdrop of Bengal Renaissance.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+This school is now managed by the West Bengal Ministry of Education and is currently situated in the district of North 24 Parganas (Kamarhati Municipality). Currently Bengali is the primary medium of instruction in this school. The school is for boys and consists of Grades from 5 to 12 and is taught as per the guidelines of the West Bengal Board of Secondary Education and the West Bengal Council of Higher Secondary Education. The academic session starts in January.
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+## Tools and Technologies
 
-### `npm test`
+* React 
+* HTML
+* CSS
+* Java Script
+* NPM
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+## Application Screenshorts
 
-### `npm run build`
+Upper Part:
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+<img src = "./Images/img1.jpeg" width="50%">
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+Lower Part:
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+<img src = "./Images/img2.jpeg" width="50%">
 
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/App.js
+++ src/App.js
@@ -1,23 +1,134 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import logo from "./kalachandLogo.png";
+import schoolimg from "./akhs.jpg";
+import school from "./ariadah-kalachand-high-school.webp";
+import school2 from "./school2.jpg";
+import school3 from "./school3.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <img src={logo} width="100px"></img>
+            Ariadaha Kalachand High School
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="col-auto">
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav me-auto">
+                <li className="nav-item ">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Gallery
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Contact
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    About
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <div className="Header">
+        <img src={schoolimg} width="1440px" className="schimg"></img>
+        <div className="square"></div>
+        <h2>Welcome to</h2>
+        <h1>Ariadaha Kalachand High School</h1>
+        <div className="square2"></div>
+        <h5>
+          For more than a century, Ariadaha Kalachand High School has been
+          standing as an epitome of the aspirations of the people of
+          Ariadaha-Dakshineswar in the northern suburb of Kolkata.The school
+          overlooks the Feeder Road that connects Belghoria Railway Station on
+          Sealdah-Ranaghat section with the Ariadaha Ferry Ghat on the riverbank
+          of the Hooghly. The junction in front of the school branches out
+          towards Dakshineswar Railway station and ultimately reaches out to the
+          Dakshineswar Kali Temple. The school has produced a plethora of
+          students who have been successful in their own fields and have
+          contributed lots to the nation in their own way. They have all done
+          the school proud.
+        </h5>
+      </div>
+      <div className="cards">
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+          <div class="col">
+            <div class="card h-60">
+              <img src={school} class="card-img-top" />
+              <div class="card-body">
+                <h5 class="card-title">Upcoming Events</h5>
+                <p class="card-text">
+                  AKHS is going to publish the annual magazine during the
+                  forthcoming Reunion scheduled in 21 March 2021.AKHS organises,
+                  like previous years, "Bijoya Sammelani"on Saturday the 19th of
+                  October 2021 at the school premises.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card h-60">
+              <img src={school2} class="card-img-top" />
+              <div class="card-body">
+                <h5 class="card-title">Foundation & History</h5>
+                <p class="card-text">
+                  AKHS was set up on 6th January, 1884 on the bank of the holy
+                  river Ganga, being inspired by the ideals of Pandit Iswar
+                  Chandra Vidyasagar and Reverened Krishnamohan Bandyopadhyay
+                  and guided by the spirit of the ‘Young Bengal' against the
+                  back drop of Bengal Renaissance.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card h-60">
+              <img src={school3} class="card-img-top" />
+              <div class="card-body">
+                <h5 class="card-title">Praktani</h5>
+                <p class="card-text">
+                  PRAKTANI, an association of the school is housed within the
+                  premises of the school for the ex- students to sip in the cup
+                  of sweet memories of their school days !!!! and nurture an
+                  emotional feeling of their companionship with the class mates.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="space"></div>
+      <div className="footer">
+        <h5>© 2021 Ariadaha Kalachand High School. All Rights Reserved.</h5>
+      </div>
     </div>
   );
 }
